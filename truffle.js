fc1d8648c199552fafb7b58499bfa332e2a7c6d7
Cyril Lapinte <cyril.lapinte@free.fr>
2017-06-03 16:41:55 +0200

    adding devchain network

--- truffle.js
+++ truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    devchain: {
+      host: "172.17.0.1",
+      port: 10001,
+      network_id: "2017042099"
     }
   }
 };
---
+++ unlock_account.js
@@ -0,0 +1,4 @@
+# unlock accounts[0] for 1000 seconds
+web3.personal.unlockAccount(web3.personal.listAccounts[0], "password", 1000);
+
+~
